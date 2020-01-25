c7bda41445ef41824c977968b9abdfc8050e876a
Arkadiy-Garber <arkg@udel.edu>
2020-01-24 20:21:08 -0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,43 +1,26 @@
-#!/usr/bin/env bash
 
-# setting colors to use
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m'
+conda create -n aladdin hmmer diamond prodigal blast --yes
 
-gzip -d HMM-lib.tar.gz
-tar xf HMM-lib.tar
-rm HMM-lib.tar
+source activate aladdin
 
-gzip -d test_dataset.tar.gz
-tar xf test_dataset.tar
-rm test_dataset.tar
-
-printf "\n    ${GREEN}Setting up conda environment...${NC}\n\n"
-
-## adding conda channels
-conda config --add channels defaults 2> /dev/null
-conda config --add channels bioconda 2> /dev/null
-conda config --add channels conda-forge 2> /dev/null
-conda config --add channels au-eoed 2> /dev/null
-
-## creating GToTree environment and installing dependencies
-conda create -n fegenie hmmer diamond prodigal blast --yes
-
-## activating environment
-source activate fegenie
-
-## creating directory for conda-env-specific source files
 mkdir -p ${CONDA_PREFIX}/etc/conda/activate.d
 
-## adding FeGenie bin path and HMM_dir variable:
+
+## adding paths:
 echo '#!/bin/sh'" \
+
 export PATH=\"$(pwd):"'$PATH'\"" \
-export rscripts=\"$(pwd)/rscripts\"
-export HMM_dir=\"$(pwd)/HMM-lib\"" >> ${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh
 
-# re-activating environment so variable and PATH changes take effect
-source activate fegenie
+export gas_hmms=\"$(pwd)/hmms/gas\"
 
+export wsp_hmms=\"$(pwd)/hmms/wsp\"
 
-printf "\n        ${GREEN}DONE!${NC}\n\n"
+export ros_hmms=\"$(pwd)/hmms/ros\"
+
+export lux_hmms=\"$(pwd)/hmms/lux\"
+
+export magneto_hmms=\"$(pwd)/hmms/magneto\"
+
+export litho_hmms=\"$(pwd)/hmms/litho\"
+
+export iron_hmms=\"$(pwd)/hmms/iron\"" >> ${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh
