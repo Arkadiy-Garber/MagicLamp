3eb8bb1a1f3773e7827e038de46eb7acf4fe8343
Arkadiy-Garber <arkg@udel.edu>
2020-01-24 20:26:00 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,25 @@
+#!/usr/bin/env bash
+
+printf "\n    ${GREEN}Setting up conda environment...${NC}\n\n"
+
+## adding conda channels
+conda config --add channels defaults 2> /dev/null
+conda config --add channels bioconda 2> /dev/null
+conda config --add channels conda-forge 2> /dev/null
+conda config --add channels au-eoed 2> /dev/null
 
 conda create -n magiccave hmmer diamond prodigal blast --yes
 
+## activating environment
 source activate magiccave
 
+## creating directory for conda-env-specific source files
 mkdir -p ${CONDA_PREFIX}/etc/conda/activate.d
-
 
 ## adding paths:
 echo '#!/bin/sh'" \
+
+export rscripts=\"$(pwd)/rscripts\"
 
 export PATH=\"$(pwd):"'$PATH'\"" \
 
@@ -24,3 +36,9 @@ export magneto_hmms=\"$(pwd)/hmms/magneto\"
 export litho_hmms=\"$(pwd)/hmms/litho\"
 
 export iron_hmms=\"$(pwd)/hmms/iron\"" >> ${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh
+
+# re-activating environment so variable and PATH changes take effect
+source activate magiccave
+
+
+printf "\n        ${GREEN}DONE!${NC}\n\n"
