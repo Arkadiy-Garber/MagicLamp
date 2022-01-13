691606a7c32650b5b0a07a9a6c16071a05368afb
Arkadiy-Garber <agarber4@asu.edu>
2022-01-13 09:59:54 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,12 +10,12 @@ conda config --add channels au-eoed 2> /dev/null
 
 conda create -n magiclamp -c r r-ggplot2 r-stringi r-reshape r-reshape2 r-tidyverse r-argparse r-ggdendro r-pvclust python=3.7 hmmer diamond prodigal blast metabat2 --yes
 
-Rscript -e 'install.packages("grid", repos = "http://cran.us.r-project.org")'
+#Rscript -e 'install.packages("grid", repos = "http://cran.us.r-project.org")'
 Rscript -e 'install.packages("broom", repos = "http://cran.us.r-project.org”)'
 Rscript -e 'install.packages("ggpubr", repos = "http://cran.us.r-project.org”)'
 
 ## activating environment
-source activate magiclamp
+conda activate magiclamp
 
 ## creating directory for conda-env-specific source files
 mkdir -p ${CONDA_PREFIX}/etc/conda/activate.d
@@ -46,7 +46,7 @@ export circ_hmms=\"$(pwd)/hmms/circ\"
 export iron_hmms=\"$(pwd)/hmms/iron\"" >> ${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh
 
 # re-activating environment so variable and PATH changes take effect
-source activate magiclamp
+conda activate magiclamp
 
 
 printf "\n        ${GREEN}DONE!${NC}\n\n"
